20147909165b8a31fd959f3e11eb26102df9ddf2
Brandonmkmk <brandonxd611@gmail.com>
2024-10-24 11:47:09 -0600

    test

--- app/(tabs)/list.js
+++ app/(tabs)/list.js
@@ -3,7 +3,7 @@ import { View,Text } from "react-native";
 export default function List(){
     return (
         <View>
-            <Text>Screen de listas</Text>
+        
         </View>
     )
 }
